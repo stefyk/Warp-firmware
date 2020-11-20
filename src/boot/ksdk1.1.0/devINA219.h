558da771ddf9ed14c45d30aedcb3686a153b2f6f
stefyk <43543157+stefyk@users.noreply.github.com>
2020-11-20 13:40:30 +0000

    Update devINA219.h

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -19,7 +19,7 @@ void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  device
 WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
 
 WarpStatus
-writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t value);
+writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload);
 //WarpStatus	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1, uint16_t menuI2cPullupValue);
 //WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
 //					WarpSignalPrecision precision,
