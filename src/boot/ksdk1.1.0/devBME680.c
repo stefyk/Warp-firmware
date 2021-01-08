9c3b944d14174093bb25d0ed2324ea2f2fe3ea32
stefyk <43543157+stefyk@users.noreply.github.com>
2021-01-08 20:02:36 +0200

    Update devBME680.c

--- src/boot/ksdk1.1.0/devBME680.c
+++ src/boot/ksdk1.1.0/devBME680.c
@@ -39,7 +39,7 @@
 #include "fsl_misc_utilities.h"
 #include "fsl_device_registers.h"
 #include "fsl_i2c_master_driver.h"
-#include "fsl_spi_master_driver.h"
+//#include "fsl_spi_master_driver.h"
 #include "fsl_rtc_driver.h"
 #include "fsl_clock_manager.h"
 #include "fsl_power_manager.h"
