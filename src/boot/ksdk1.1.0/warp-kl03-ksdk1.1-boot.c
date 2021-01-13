18a2e594be81447effa9821c977683b2aa22bdbd
stefyk <43543157+stefyk@users.noreply.github.com>
2021-01-13 12:56:59 +0200

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -2688,9 +2688,6 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag, int menuDelay
 		#endif
 		#ifdef WARP_BUILD_ENABLE_DEVCCS811
 		printSensorDataCCS811(hexModeFlag);
-		int16_t		threshold;
-		bool  		aboveThreshold;
-		threshold	= 1000;
 		#endif
 		#ifdef WARP_BUILD_ENABLE_DEVHDC1000
 		printSensorDataHDC1000(hexModeFlag);
