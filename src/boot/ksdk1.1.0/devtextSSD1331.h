2eb699876321654e08b03f9685943ac7ede71c4d
stefyk <43543157+stefyk@users.noreply.github.com>
2021-01-14 17:37:00 +0200

    Update devtextSSD1331.h

--- src/boot/ksdk1.1.0/devtextSSD1331.h
+++ src/boot/ksdk1.1.0/devtextSSD1331.h
@@ -1,5 +1,10 @@
  
  
+//this file was taken from os.mbed.com (and adapted), full link: https://os.mbed.com/users/star297/code/ssd1331/file/4385fd242db0/ssd1331.h/
+
+
+#pragma once
+
 // Screen Settings
 #define width   96-1        // Max X axial direction in screen
 #define height  64-1        // Max Y axial direction in screen
@@ -10,7 +15,7 @@
 #define contrastC           0x83
 #define display_on          0xAF
 #define display_off         0xAE
- 
+
 // Internal Font size settings
 #define NORMAL  0
 #define WIDE    1
@@ -18,9 +23,9 @@
 #define WH      3
 #define WHx36   4
 #define X_width 6 
-#define Y_height 8 
- 
- 
+#define Y_height 16 
+
+
 // GAC hardware acceleration commands
 #define GAC_DRAW_LINE           0x21    // Draw Line
 #define GAC_DRAW_RECTANGLE      0x22    // Rectangle
@@ -31,9 +36,9 @@
 #define SCROLL_SETUP            0x27    // Setup scroll
 #define SCROLL_STOP             0x2E    // Scroll Stop
 #define SCROLL_START            0x2F    // Scroll Start
- 
+
 // Basic RGB color definitions         RED GREEN BLUE values                         
- 
+
 #define Black           0x0000      //   0,   0,   0 
 #define LightGrey       0xC618      // 192, 192, 192 
 #define DarkGrey        0x7BEF      // 128, 128, 128 
@@ -44,19 +49,22 @@
 #define Magenta         0xF81F      // 255,   0, 255 
 #define Yellow          0xFFE0      // 255, 255,   0 
 #define White           0xFFFF      // 255, 255, 255 
- 
+
+#include <stdint.h>
+
+
 // example code
 /*
 #include "mbed.h"
 #include "ssd1331.h"
- 
+
 ssd1331 oled(D8, D9, D10, D11, NC, D13); // cs, res, dc, miso(nc), sck (KL25z)
- 
+
 char Time[50],Date[50];
 void gettime();
- 
+
 uint8_t main() { 
- 
+
     while(1){
         
         oled.Fill_Screen(oled.toRGB(255,0,0)); //red
@@ -90,7 +98,7 @@ uint8_t main() {
         oled.contrast(9); // set contrast to maximum
         wait_ms(2000);
         oled.cls();
- 
+
         oled.SetFontSize(HIGH); // set tall font
         oled.foreground(oled.toRGB(0,255,0)); // set text colour
         oled.locate(0, 10);
@@ -140,42 +148,19 @@ void gettime()
     oled.printf(Time); 
 }
 */
- 
-class ssd1331 : public Stream {
-public:
-    // constructor
-    ssd1331(PinName cs_pin, PinName rst_pin, PinName a0_pin, PinName mosi_pin, PinName miso_pin, PinName sclk_pin);
-    
-    void Init(void);
+
+
     void pixel(uint8_t x,uint8_t y, uint16_t color); // place a pixel x,y coordinates, color
-    void rect(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint16_t colorline); // draw rectangle, start x,y end x,y, color
-    void fillrect(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint16_t colorline,uint16_t colorfill); // fill rectangle start x,y, end x,y, outline color, fill color.
     void line(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint16_t color); // draw line start x,y, end x,y, color
-    void circle (uint8_t radius, uint8_t x, uint8_t y,uint16_t color,uint16_t fill); // draw circle radius, position x,y, color
-    void Fill_Screen(uint16_t color); // fill screen with any colour
     void foreground(uint16_t color); // set text color
     void background(uint16_t color); // set background color
     void SetFontSize(uint8_t); // set internal font size NORMAL, HIGH, WIDE, WH (high and wide), WHx36 (large 36x36 pixel size)
-    void on();  // display on
-    void off(); // display off
-    void cls(); // clear screen to black screen
-    void dim();  // flip dim/normal on each call
-    void contrast(char value); //0~9 low~high
     void locate(uint8_t column, uint8_t row); // text start position x,y
     uint16_t toRGB(uint16_t R,uint16_t G,uint16_t B);   // get color from RGB values 00~FF(0~255)
     uint8_t row(); // set row position (in pixels)
     uint8_t column(); // set column position (in pixels)
-    void ScrollSet(int8_t horizontal, int8_t startline, int8_t linecount, int8_t vertical , int8_t frame_interval); // set up scroll function
-    void Scrollstart(); // start scrolling
-    void Scrollstop(); // stop scrolling
-    void Copy(uint8_t src_x1,uint8_t src_y1,uint8_t src_x2,uint8_t src_y2, uint8_t dst_x,uint8_t dst_y); // GAC function to copy/paste area on the screen
-    void character(uint8_t x, uint8_t y, uint16_t c); // Print single character, x & y pixel co-ords.
-    void set_font(unsigned char* f); // set external font. Use oled.set_font(NULL) to restore default font
-    void Bitmap(const uint8_t *bitmap, uint8_t x, uint8_t y, uint8_t w, uint8_t h, unsigned char color); // load mono bitmap from flash
-    void Bitmap16(uint8_t x, uint8_t y, uint8_t w, uint8_t h, const uint8_t *bitmap); // load bitmap from flash
+    void reset_cursor();
     
-    int Bitmap16RAM(uint8_t x, uint8_t y, unsigned char *Name_BMP); // copy image to RAM, uses up to 12.288k RAM, Fast but need plenty of RAM
-    int Bitmap16FS(uint8_t x, uint8_t y, unsigned char *Name_BMP); // load from fielsystem, uses 96b RAM, slower, will work on any MCU
     // use GIMP to generate images, open image, select image-scale image, set width/height(max 96x64), select File-Export As
     // select Windows BMP image, Name it with .BMP extension, select export, select Compatability Options- check 'Do Not write colour space'
     // select Advanced Options- check '16bit R5 G6 B5', select export. Copy the file to a SD card or to the local file system. 
@@ -184,24 +169,13 @@ public:
     // oled.Bitmap16FS(0,0,(unsigned char *)"/sd/image.bmp"); full size 96x64 image
     // oled.Bitmap16RAM(20,20,(unsigned char *)"/sd/image.bmp"); place smaller image starting x-20, y-20
    
-protected:
+
     // Stream implementation functions
-    virtual int _putc(int c);
-    virtual int _getc();
-private:
-    void RegWrite(unsigned char Command);
-    void RegWriteM(unsigned char *Command, uint8_t count);
-    void DataWrite(unsigned char c);
-    void DataWrite_to(uint16_t Dat);
     void FontSizeConvert(int *lpx, int *lpy);
     void PutChar(uint8_t x,uint8_t y,int a);
-    void PutCharInt(uint8_t x,uint8_t y,uint16_t a);
-    void putp(int colour);
     unsigned char* font;
     uint16_t Char_Color;    // text color
     uint16_t BGround_Color; // background color
-    void window(uint8_t x, uint8_t y, uint8_t w, uint8_t h);
-    void Maxwindow(); // reset display window to full size
     // pixel location
     uint8_t _x;
     uint8_t _y;
@@ -216,9 +190,5 @@ private:
     uint8_t chr_size;
     uint8_t cwidth;       // character's width
     uint8_t cvert;        // character's height
-    uint8_t externalfont;
-    DigitalOut  CS,  RES,  DC;
-    SPI spi; // mosi, miso, sclk    
-};
- 
-#endif
+   
+
